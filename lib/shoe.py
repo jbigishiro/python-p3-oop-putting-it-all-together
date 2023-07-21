991fb6d249dddd1fbcb0d2ed35778298119f32eb
Justin Bigishiro <justin.rvd@gmail.com>
2023-07-21 14:22:00 -0500

    finish lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand=brand
+        self.size=size
+        self.condition="New"
+
+    def get_shoe_size(self):
+        return self._size
+    
+    def set_shoe_size(self, size):
+        if type(size)is int:
+            self._size=size
+        else:
+            print("size must be an integer")
+
+    size=property(get_shoe_size, set_shoe_size)
+
+    def cobble(self):
+        self.condition="New"
+        print( "Your shoe is as good as new!")
